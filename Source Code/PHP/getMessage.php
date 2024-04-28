14f5a4ee9c0e3547c9607d29dc06ec3aabb74455
Leinard-A <115931518+Leinard-A@users.noreply.github.com>
2024-04-28 20:41:18 +0100

    Making it easier to read, and changing file name

--- Source Code/PHP/getMessage.php
+++ Source Code/PHP/getMessage.php
@@ -4,16 +4,19 @@ $mysqli = require __DIR__. "/database.php";
 $sessionID = $_GET['sessionID'];
 $sqlEmail ="SELECT email FROM userSessions
             WHERE sessionID = '$sessionID'";
+
 //Get the user email address from session ID 
 $sqlResult = mysqli_query($mysqli, $sqlEmail);
 $result = mysqli_fetch_assoc($sqlResult);
 $userEmail = $result["email"];
 mysqli_free_result($sqlResult);
+
 //Get messages that are being sent to the email
 $sqlGetMessags ="SELECT * FROM userMessages
                 WHERE recipientEmail = '$userEmail'";
 $sqlMessages = mysqli_query($mysqli, $sqlGetMessags);
 $resultMessages = mysqli_fetch_assoc($sqlMessages);
+
 print(json_encode($resultMessages));
 mysqli_free_result($sqlMessages);
 mysqli_close($mysqli);
