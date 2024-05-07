44d80f5bd7c20d0cb84e0ed75caadb87b620e864
Leinard-A <115931518+Leinard-A@users.noreply.github.com>
2024-05-07 13:57:42 +0100

    Correcting validation

--- Source Code/PHP/getMessages.php
+++ Source Code/PHP/getMessages.php
@@ -15,9 +15,16 @@ mysqli_free_result($sqlResult);
 $sqlGetMessags ="SELECT * FROM userMessages
                 WHERE recipientEmail = '{$userEmail}'";
 $sqlMessages = mysqli_query($mysqli, $sqlGetMessags);
-$resultMessages = mysqli_fetch_assoc($sqlMessages);
-
-print(json_encode($resultMessages));
+$resultMessages = mysqli_fetch_all($sqlMessages);
+if ($resultMessages == null){
+    $message = array(
+        "NoMessages"=> True
+    );
+    print(json_encode($message));
+}
+else{
+    print(json_encode($resultMessages));
+}
 mysqli_free_result($sqlMessages);
 mysqli_close($mysqli);
 
